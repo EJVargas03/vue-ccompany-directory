ac546fbb28f9e614c1b902f47508212de721a9fa
EJVargas03 <143569160+EJVargas03@users.noreply.github.com>
2023-09-26 21:09:01 -0500

    added .env file

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@
 
   const { isAuthenticated, logout, user } = useAuth()
 
-  const brand = ref(' EJV.co Employee Directory ')
+  const brand = ref(import.meta.env.VITE_APP_NAME)
 </script>
 
 <template>
