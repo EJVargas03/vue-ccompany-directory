78296a86ed67c14f69c48aef4b9de2ea2bfb2a83
EJVargas03 <143569160+EJVargas03@users.noreply.github.com>
2023-10-01 14:25:50 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-ccompany-directory/',
+  base: '/vue-company-directory/',
   plugins: [vue()],
   resolve: {
     alias: {
